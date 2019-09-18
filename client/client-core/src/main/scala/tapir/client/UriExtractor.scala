9411dceec2cbae50d42f394b7f9349385d78b5a2
chills <contagnas@gmail.com>
2019-09-18 00:19:52 -0500

    Create generic URI and request metadata from endpoint and inputs

--- client/client-core/src/main/scala/tapir/client/UriExtractor.scala
+++ client/client-core/src/main/scala/tapir/client/UriExtractor.scala
@@ -1,24 +1,23 @@
 package tapir.client
 
-
 import java.net.URI
 
 import tapir.Codec.PlainCodec
-import tapir.{ByteArrayValueType, ByteBufferValueType, CodecForOptional, CodecMeta, Endpoint, EndpointIO, EndpointInput, FileValueType, InputStreamValueType, MediaType, MultipartValueType, RawPart, StringValueType}
 import tapir.internal._
-import tapir.model.{MultiQueryParams, Part}
+import tapir.model.MultiQueryParams
+import tapir.{CodecForOptional, Endpoint, EndpointIO, EndpointInput, MediaType}
 
 object UriExtractor {
 
   private implicit class RichUri(uri: URI) {
     def copy(
-      scheme: String = uri.getScheme,
-      userInfo: String = uri.getUserInfo,
-      host: String = uri.getHost,
-      port: Int = uri.getPort,
-      path: String = uri.getPath,
-      query: String = uri.getQuery,
-      fragment: String = uri.getFragment
+        scheme: String = uri.getScheme,
+        userInfo: String = uri.getUserInfo,
+        host: String = uri.getHost,
+        port: Int = uri.getPort,
+        path: String = uri.getPath,
+        query: String = uri.getQuery,
+        fragment: String = uri.getFragment
     ): URI = new URI(scheme, userInfo, host, port, path, query, fragment)
 
     def update(updatedComponents: UriComponent): URI = this.copy(
@@ -27,9 +26,12 @@ object UriExtractor {
       host = sOrNull(updatedComponents.host.getOrElse(uri.getHost)),
       port = updatedComponents.port.getOrElse(uri.getPort),
       path = sOrNull((sOrEmpty(uri.getPath) :: updatedComponents.path).mkString("/")),
-      query = sOrNull((queryToMap(uri.getQuery) ++ updatedComponents.query)
-        .map { case (k, v) => s"$k=$v" }.mkString("&")),
-      fragment = sOrNull(updatedComponents.fragment.getOrElse(uri.getFragment)),
+      query = sOrNull(
+        (queryToMap(uri.getQuery) ++ updatedComponents.query)
+          .map { case (k, v) => s"$k=$v" }
+          .mkString("&")
+      ),
+      fragment = sOrNull(updatedComponents.fragment.getOrElse(uri.getFragment))
     )
   }
 
@@ -43,77 +45,47 @@ object UriExtractor {
       if piece.nonEmpty
       Array(k, v) = piece.split("=")
     } yield (k, v)
-    }.toMap
+  }.toMap
+
+  sealed trait Body
+
+  case class StreamBody(s: Any) extends Body
+
+  case class NormalBody(v: Any, codec: CodecForOptional[Any, MediaType, Any]) extends Body
 
   case class UriComponent(
-    scheme: Option[String] = None,
-    userInfo: Option[String] = None,
-    host: Option[String] = None,
-    port: Option[Int] = None,
-    path: List[String] = List.empty,
-    query: Map[String, String] = Map.empty,
-    fragment: Option[String] = None
+      scheme: Option[String] = None,
+      userInfo: Option[String] = None,
+      host: Option[String] = None,
+      port: Option[Int] = None,
+      path: List[String] = List.empty,
+      query: Map[String, String] = Map.empty,
+      fragment: Option[String] = None
   )
 
   case class RequestMetadata(
-    cookies: Map[String, String] = Map.empty
+      cookies: Map[String, String] = Map.empty,
+      body: Option[Body] = None,
+      header: Map[String, String] = Map.empty,
+      method: String = "GET"
   )
 
   private def paramsTupleToParams[I](params: I): Vector[Any] = ParamsToSeq(params).toVector
 
-  private def setBody[T, M <: MediaType, R](v: T, codec: CodecForOptional[T, M, R], req: PartialAnyRequest): PartialAnyRequest = {
-    codec
-      .encode(v)
-      .map { t =>
-        val req2 = codec.meta.rawValueType match {
-          case StringValueType(charset) => req.body(t, charset.name())
-          case ByteArrayValueType       => req.body(t)
-          case ByteBufferValueType      => req.body(t)
-          case InputStreamValueType     => req.body(t)
-          case FileValueType            => req.body(t)
-          case mvt: MultipartValueType =>
-            val parts: Seq[Multipart] = (t: Seq[RawPart]).flatMap { p =>
-              mvt.partCodecMeta(p.name).map { partCodecMeta =>
-                val sttpPart1 = partToSttpPart(p.asInstanceOf[Part[Any]], partCodecMeta.asInstanceOf[CodecMeta[_, _, Any]])
-                val sttpPart2 = sttpPart1.contentType(partCodecMeta.mediaType.mediaTypeNoParams)
-                val sttpPart3 = p.headers.foldLeft(sttpPart2) {
-                  case (sp, (hk, hv)) =>
-                    if (hk.equalsIgnoreCase(HeaderNames.ContentType)) {
-                      sp.contentType(hv)
-                    } else {
-                      sp.header(hk, hv)
-                    }
-                }
-                p.fileName.map(sttpPart3.fileName).getOrElse(sttpPart3)
-              }
-            }
-
-            req.multipartBody(parts.toList)
-        }
-
-        req2.header(HeaderNames.ContentType, codec.meta.mediaType.mediaType, replaceExisting = false)
-      }
-      .getOrElse(req)
-  }
-
   def extractUri[I, E, O, S](baseUri: URI, endpoint: Endpoint[I, E, O, S], params: I): URI = {
-
-    private def setInputParams[I](
-      inputs: Vector[EndpointInput.Single[_]],
-      params: Vector[Any],
-      paramIndex: Int,
-      uri: Uri,
-      req: PartialAnyRequest
-    ): (Uri, PartialAnyRequest)
-
-    =
-    {
+    def setInputParams(
+        inputs: Vector[EndpointInput.Single[_]],
+        params: Vector[Any],
+        paramIndex: Int,
+        uri: URI,
+        req: RequestMetadata
+    ): (URI, RequestMetadata) = {
       def handleMapped[II, T](
-        wrapped: EndpointInput[II],
-        g: T => II,
-        tail: Vector[EndpointInput.Single[_]]
-      ): (Uri, PartialAnyRequest) = {
-        val (uri2, req2) = setInputParams(
+          wrapped: EndpointInput[II],
+          g: T => II,
+          tail: Vector[EndpointInput.Single[_]]
+      ): (URI, RequestMetadata) = {
+        val (uriParts2, req2) = setInputParams(
           wrapped.asVectorOfSingleInputs,
           paramsTupleToParams(g(params(paramIndex).asInstanceOf[T])),
           0,
@@ -121,7 +93,7 @@ object UriExtractor {
           req
         )
 
-        setInputParams(tail, params, paramIndex + 1, uri2, req2)
+        setInputParams(tail, params, paramIndex + 1, uriParts2, req2)
       }
 
       inputs match {
@@ -129,49 +101,49 @@ object UriExtractor {
         case EndpointInput.FixedMethod(_) +: tail =>
           setInputParams(tail, params, paramIndex, uri, req)
         case EndpointInput.FixedPath(p) +: tail =>
-          setInputParams(tail, params, paramIndex, uri.copy(path = uri.path :+ p), req)
+          setInputParams(tail, params, paramIndex, uri.update(UriComponent(path = List(p))), req)
         case EndpointInput.PathCapture(codec, _, _) +: tail =>
           val v = codec.asInstanceOf[PlainCodec[Any]].encode(params(paramIndex): Any)
-          setInputParams(tail, params, paramIndex + 1, uri.copy(path = uri.path :+ v), req)
+          setInputParams(tail, params, paramIndex + 1, uri.update(UriComponent(path = List(v))), req)
         case EndpointInput.PathsCapture(_) +: tail =>
           val ps = params(paramIndex).asInstanceOf[Seq[String]]
-          setInputParams(tail, params, paramIndex + 1, uri.copy(path = uri.path ++ ps), req)
+          setInputParams(tail, params, paramIndex + 1, uri.update(UriComponent(path = ps.toList)), req)
         case EndpointInput.Query(name, codec, _) +: tail =>
-          val uri2 = codec
+          val query: Map[String, String] = codec
             .encode(params(paramIndex))
-            .foldLeft(uri) { case (u, v) => u.param(name, v) }
+            .map(v => name -> v)
+            .toMap
+          val uri2 = uri.update(UriComponent(query = query))
           setInputParams(tail, params, paramIndex + 1, uri2, req)
         case EndpointInput.Cookie(name, codec, _) +: tail =>
           val req2 = codec
             .encode(params(paramIndex))
-            .foldLeft(req) { case (r, v) => r.cookie(name, v) }
+            .foldLeft(req) { case (r, v) => r.copy(cookies = r.cookies + (name -> v)) }
           setInputParams(tail, params, paramIndex + 1, uri, req2)
         case EndpointInput.QueryParams(_) +: tail =>
           val mqp = params(paramIndex).asInstanceOf[MultiQueryParams]
-          val uri2 = uri.params(mqp.toSeq: _*)
+          val uri2 = uri.update(UriComponent(query = mqp.toMap))
           setInputParams(tail, params, paramIndex + 1, uri2, req)
-        case EndpointIO.Body(codec, _) +: tail =>
-          val req2 = setBody(params(paramIndex), codec, req)
+        case EndpointIO.Body(codec: CodecForOptional[Any, MediaType, Any], _) +: tail =>
+          val req2 = req.copy(body = Some(NormalBody(params(paramIndex), codec)))
           setInputParams(tail, params, paramIndex + 1, uri, req2)
         case EndpointIO.StreamBodyWrapper(_) +: tail =>
-          val req2 = req.streamBody(params(paramIndex))
+          val req2 = req.copy(body = Some(StreamBody(params(paramIndex))))
           setInputParams(tail, params, paramIndex + 1, uri, req2)
         case EndpointIO.Header(name, codec, _) +: tail =>
-          val req2 = codec
+          val headers = codec
             .encode(params(paramIndex))
-            .foldLeft(req) { case (r, v) => r.header(name, v) }
+            .map(name -> _)
+            .toMap
+          val req2 = req.copy(header = headers)
           setInputParams(tail, params, paramIndex + 1, uri, req2)
         case EndpointIO.Headers(_) +: tail =>
           val headers = params(paramIndex).asInstanceOf[Seq[(String, String)]]
-          val req2 = headers.foldLeft(req) {
-            case (r, (k, v)) =>
-              val replaceExisting = HeaderNames.ContentType.equalsIgnoreCase(k) || HeaderNames.ContentLength.equalsIgnoreCase(k)
-              r.header(k, v, replaceExisting)
-          }
+          val req2 = req.copy(header = headers.toMap)
           setInputParams(tail, params, paramIndex + 1, uri, req2)
         case EndpointIO.FixedHeader(name, value, _) +: tail =>
-          val req2 = Seq(value)
-            .foldLeft(req) { case (r, v) => r.header(name, v) }
+          val header = Map(name -> value)
+          val req2 = req.copy(header = header)
           setInputParams(tail, params, paramIndex, uri, req2)
         case EndpointInput.ExtractFromRequest(_) +: tail =>
           // ignoring
@@ -184,5 +156,7 @@ object UriExtractor {
           handleMapped(wrapped, g, tail)
       }
     }
+
+    setInputParams(endpoint.input.asVectorOfSingleInputs, paramsTupleToParams(params), 0, baseUri, RequestMetadata())._1
   }
 }
